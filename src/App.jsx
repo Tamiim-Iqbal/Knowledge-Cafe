a2b12f03867b213ff2a22d20c2baf132d6214d83
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2024-10-25 18:12:03 +0600

    Resources add, Blogs Component, Fetch Fake Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <>
-      <h1 className='text-4xl font-semibold'>Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
