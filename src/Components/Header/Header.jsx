8b26a7117e0dbe63634750e26099ac2d7b6ed721
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2024-10-26 11:18:20 +0600

    Image Upload Online Site

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
     return (
         <div className="flex justify-between items-center pt-3 pb-3 border-b-2">
             <h1 className='text-4xl font-semibold'>Knowledge Cafe</h1>
-            <img src="../../../resources/images/profile.png" alt="" />
+            <img src="https://i.ibb.co.com/jktDrJv/profile.png" alt="" />
         </div>
     );
 };
